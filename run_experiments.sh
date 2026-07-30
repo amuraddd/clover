9dea4a755fb1da4e0822aaba822b8ecb662508d1
Ali Murad <alimuradd7@gmail.com>
2026-07-30 14:52:02 -0500

    update

--- run_experiments.sh
+++ run_experiments.sh
@@ -22,7 +22,6 @@ workdir=/aiau010_scratch/azm0269/clover
 cd $workdir
 source .env
 HF_HOME="/aiau010_scratch/azm0269/hub"
-HF_TOKEN=
 HF_HOME="/aiau010_scratch/azm0269/hub"
 TMPDIR="/aiau010_scratch/azm0269/tmp"
 # CUDA_VISIBLE_DEVICES=6,7
